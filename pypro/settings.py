a2b7ed7993da18e0266f6206175ba61dc79a967a
victorsmorais <victorsanmorais@gmail.com>
2021-08-27 18:35:13 -0300

    config DEBUG alterado

--- pypro/settings.py
+++ pypro/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dlh+_xbh*hcwo^+5&061wzshdgihvsb@@!el1%cw$0)&ct!wsp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
