28eff586c1d6f0d8159d58f179ac198aa1389e17
victorsmorais <victorsanmorais@gmail.com>
2021-09-14 01:07:14 -0300

    templates com slug

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    videos = {
+         "minnie-the-moocher": {'titulo': 'Minnie The Moocher', 'vimeo_id': '603606638?h=d8f7b448cc'},
+         "snow-white": {'titulo': 'Snow White', 'vimeo_id': '604460418?h=f9fbd0a002'},
+    }
+    video = videos[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
