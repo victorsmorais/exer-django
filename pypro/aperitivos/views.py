c03cfc172eac75852134f08afd405f7703d6198a
victorsmorais <victorsanmorais@gmail.com>
2021-09-18 01:23:34 -0300

    Templates terminados

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
+from django.urls import reverse
+
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.vimeo_id = vimeo_id
+        self.titulo = titulo
+        self.slug = slug
+
+    def get_absolute_url(self):
+        return reverse('aperitivos:video', args=(self.slug,))
+
+
+videos = [
+    Video('minnie-the-moocher', 'Minnie The Moocher', '603606638?h=d8f7b448cc'),
+    Video('snow-white', 'Snow White', '604460418?h=f9fbd0a002'),
+]
+
+videos_dct = {v.slug: v for v in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
-    videos = {
-         "minnie-the-moocher": {'titulo': 'Minnie The Moocher', 'vimeo_id': '603606638?h=d8f7b448cc'},
-         "snow-white": {'titulo': 'Snow White', 'vimeo_id': '604460418?h=f9fbd0a002'},
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
