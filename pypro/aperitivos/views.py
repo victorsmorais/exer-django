6334caa79946102e98217485d53759a0431ef8ad
victorsmorais <victorsanmorais@gmail.com>
2021-09-20 01:10:48 -0300

    testes

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,20 +1,10 @@
-from django.shortcuts import render
-from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
 
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.vimeo_id = vimeo_id
-        self.titulo = titulo
-        self.slug = slug
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
-
+from pypro.aperitivos.models import Video
 
 videos = [
-    Video('minnie-the-moocher', 'Minnie The Moocher', '603606638?h=d8f7b448cc'),
-    Video('snow-white', 'Snow White', '604460418?h=f9fbd0a002'),
+    Video(slug='minnie-the-moocher', titulo='Minnie The Moocher', vimeo_id='603606638?h=d8f7b448cc'),
+    Video(slug='snow-white', titulo='Snow White', vimeo_id='604460418?h=f9fbd0a002'),
 ]
 
 videos_dct = {v.slug: v for v in videos}
@@ -25,5 +15,5 @@ def indice(request):
 
 
 def video(request, slug):
-    video = videos_dct[slug]
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
