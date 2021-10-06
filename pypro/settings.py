e191f624c285483ae16025793acc61b151ded7a9
victorsmorais <victorsanmorais@gmail.com>
2021-10-05 23:25:49 -0300

    testes funcionando

--- pypro/settings.py
+++ pypro/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'pypro.base',
     'pypro.aperitivos',
     'pypro.modulos',
+    'ordered_model',
 ]
 
 MIDDLEWARE = [
@@ -72,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'pypro.modulos.context_processors.listar_modulos',
             ],
         },
     },
