e09989b7a3edf97f2f4c6eb3d8d393f1ebe06bb0
victorsmorais <victorsanmorais@gmail.com>
2021-10-19 23:03:39 -0300

    vimeo_id adicionado

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render # noqa
+from django.shortcuts import render
 
 from pypro.modulos import facade
 
@@ -7,3 +7,8 @@ def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
     aulas = facade.listar_aulas_ordenadas(modulo)
     return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
+
+
+def aula(request, slug):
+    aula = facade.encontrar_aula(slug)
+    return render(request, 'modulos/aula_detalhe.html', {'aula': aula})
