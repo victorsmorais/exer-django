b77dba259625c7f440b3117632c564d9b7b2b795
victorsmorais <victorsanmorais@gmail.com>
2021-12-14 17:37:29 -0300

    botão 'Sair' implementado

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -58,3 +58,11 @@ def test_botao_entrar_indisp(resp_home_usuario_logado):
 
 def test_link_login_indisponivel(resp_home_usuario_logado):
     assert_not_contains(resp_home_usuario_logado, reverse('login'))
+
+
+def test_botao_sair_disp(resp_home_usuario_logado):
+    assert_contains(resp_home_usuario_logado, 'Sair')
+
+
+def test_nome_usuario_disp(resp_home_usuario_logado, usuario_logado):
+    assert_contains(resp_home_usuario_logado, usuario_logado.first_name)
--- pypro/conftest.py
+++ pypro/conftest.py
@@ -4,7 +4,7 @@ from model_bakery import baker
 
 @pytest.fixture
 def usuario_logado(db, django_user_model):
-    usuario_modelo = baker.make(django_user_model)
+    usuario_modelo = baker.make(django_user_model, first_name='nome')
     return usuario_modelo
 
 
